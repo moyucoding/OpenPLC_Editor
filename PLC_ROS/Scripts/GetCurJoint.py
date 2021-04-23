a5e729cc444068ad6a0e0ec1b0dadd2db6548273
moyucoding <tharealzhong@gmail.com>
2021-04-23 09:51:04 +0800

    20210423
    FB MoveAbsJoint,GetCurJoint -- ROS2

--- PLC_ROS/Scripts/GetCurJoint.py
+++ PLC_ROS/Scripts/GetCurJoint.py
@@ -1,62 +1,25 @@
 import os
 import time
-
-from action_msgs.msg import GoalStatus
-from example_interfaces.action import Fibonacci
-
 import rclpy
-from rclpy.action import ActionClient
 from rclpy.node import Node
 
+from ros_motion.msg import MotionGetCurJoint
 
-class GetCurJointActionClient(Node):
+
+class GetCurJointClient(Node):
 
     def __init__(self):
-        super().__init__('getcurjoint_action_client')
-        
-        self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
-        self._action_result = ''
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            MotionGetCurJoint, 'getcurjoint',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self._result = ''
 
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
-            return
-
-        self.get_logger().info('Goal accepted :)')
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    def feedback_callback(self, feedback):
-        self.get_logger().info('Received feedback: {0}'.format(feedback.feedback.sequence))
-
-    def get_result_callback(self, future):
-        result = future.result().result
-        status = future.result().status
-        if status == GoalStatus.STATUS_SUCCEEDED:
-            self.get_logger().info('Goal succeeded! Result: {0}'.format(result.sequence))
-        else:
-            self.get_logger().info('Goal failed with status: {0}'.format(status))
-        # Shutdown after receiving a result
-        self._action_result = result
-        rclpy.shutdown()
-
-    def send_goal(self):
-        self.get_logger().info('Waiting for action server...')
-        self._action_client.wait_for_server()
-
-        goal_msg = Fibonacci.Goal()
-        goal_msg.order = 10
-
-        self.get_logger().info('Sending goal request...')
-        self._send_goal_future = self._action_client.send_goal_async(
-            goal_msg,
-            feedback_callback=self.feedback_callback)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-
-
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.joint)
+        self._result = msg.joint
 
 
 class GetCurJointHandler():
@@ -82,16 +45,17 @@ class GetCurJointHandler():
                     # 
                     
                     rclpy.init()
-                    self.ros_handler = GetCurJointActionClient()
-                    self.ros_handler.send_goal()
-                    rclpy.spin(self.ros_handler)
+                    self.ros_handler = GetCurJointClient()
+                    rclpy.spin_once(self.ros_handler)
+                    
+                    
                     print('[Get]  GetCurJoint result.')
-                    print(self.ros_handler._action_result.sequence[-1])
+                    print(self.ros_handler._result)
                     #Get data from ROS topic
                     #Valid
-                    axises = str(self.ros_handler._action_result.sequence[-1])
-                    axises += '.0,1.1,2.2,3.3,4.4,5.5,'
-                    validcode = 'y' + axises
+                    joints = [float(x) for x in self.ros_handler._result]
+                    str_joints = [str(x) for x in joints]
+                    validcode = 'y' + ','.join(str_joints) + ','
                     validcode += ' '*(100-len(validcode))
                     #Error
                     errorcode = 'n1'
--- PLC_ROS/Scripts/MoveAbsJoint.py
+++ PLC_ROS/Scripts/MoveAbsJoint.py
@@ -1,22 +1,34 @@
 import os
 import time
 
-
-from action_msgs.msg import GoalStatus
-from example_interfaces.action import Fibonacci
-
 import rclpy
-from rclpy.action import ActionClient
 from rclpy.node import Node
-
+from rclpy.action import ActionClient
+from ros_motion.action import MotionMoveAbsJoint
 
 class MoveAbsJointActionClient(Node):
 
     def __init__(self):
-        super().__init__('moveabsjoint_action_client')
+        super().__init__('motionmoveabsjoint_action_client')
         
-        self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
-        self._action_result = False
+        self._action_client = ActionClient(self, MotionMoveAbsJoint, 'motionmoveabsjoint')
+        self._action_result = 0
+
+    def send_goal(self, goal_msg):
+        # goal_msg = MotionMoveAbsJoint.Goal()
+
+        # goal_msg.id = 1
+        # goal_msg.joint = [1.1,2.2,3.3,4.4,5.5,6.6]
+        # goal_msg.extjoint = [0.0,0.0,0.0,0.0,0.0,0.0]
+        # goal_msg.load = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        # goal_msg.speed = 0.5
+        # goal_msg.zone = 0.5
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(
+            goal_msg, feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
@@ -28,34 +40,16 @@ class MoveAbsJointActionClient(Node):
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
-    def feedback_callback(self, feedback):
-        self.get_logger().info('Received feedback: {0}'.format(feedback.feedback.sequence))
-
     def get_result_callback(self, future):
         result = future.result().result
-        status = future.result().status
-        if status == GoalStatus.STATUS_SUCCEEDED:
-            self.get_logger().info('Goal succeeded! Result: {0}'.format(result.sequence))
-            self._action_result = True
-        else:
-            self.get_logger().info('Goal failed with status: {0}'.format(status))
-            self._action_result = False
-        # Shutdown after receiving a result
+        self.get_logger().info('Get Result :(')
+        self.get_logger().info('Result: {0}'.format(result.ret))
+        self._action_result = int(result.ret)
         rclpy.shutdown()
 
-    def send_goal(self,goal):
-        self.get_logger().info('Waiting for action server...')
-        self._action_client.wait_for_server()
-
-        goal_msg = Fibonacci.Goal()
-        goal_msg.order = goal
-
-        self.get_logger().info('Sending goal request...')
-        self._send_goal_future = self._action_client.send_goal_async(
-            goal_msg,
-            feedback_callback=self.feedback_callback)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.cnt))
 
 
 
@@ -79,10 +73,22 @@ class MoveAbsJointHandler():
                     print('[Get]  MoveAbsJoint request.')
                     #print(data.decode('utf-8'))
                     request = data.decode('utf-8')
-                    goal = int(float(request.split(',')[1])//1)
+                    #goal = int(float(request.split(',')[1])//1)
+                    print('[Request]: ',request)
                     #
                     # Do Something
                     # 
+
+                    #Create a goal
+                    request_msg = request.split(',')
+                    goal = MotionMoveAbsJoint.Goal()
+                    goal.id = 1
+                    goal.joint = [float(x) for x in request_msg[1:7]]
+                    goal.extjoint = [float(x) for x in request_msg[7:13]]
+                    goal.load = [float(x) for x in request_msg[13:23]]
+                    goal.speed = float(request_msg[23])
+                    goal.zone = float(request_msg[24])
+                    
                     rclpy.init()
                     self.ros_handler = MoveAbsJointActionClient()
                     self.ros_handler.send_goal(goal)
