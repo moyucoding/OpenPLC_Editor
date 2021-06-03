f414df7c43e206df9b72b0fb028135271de9ab2a
moyucoding <tharealzhong@gmail.com>
2021-06-03 15:40:30 +0800

    210603_v2
    Bug Fixed

--- matiec/lib/C/iec_std_FB.h
+++ matiec/lib/C/iec_std_FB.h
@@ -2299,7 +2299,7 @@ static void GETCURSTATE_body__(GETCURSTATE *data__) {
   }
   
   char buf[20];
-  int ret = read(fd, buf, 20;
+  int ret = read(fd, buf, 20）;
   if(ret > 0){
     int res =  buf[0] - 48;
     if(0 <= res && res <= 5){
